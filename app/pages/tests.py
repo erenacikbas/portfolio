23c34646c5343db51223138e6d7c765d5afe7b3a
Eren Tuna Açıkbaş <erentuna26@icloud.com>
2022-07-08 21:38:58 +0300

    portfolio page updated.

--- app/pages/tests.py
+++ app/pages/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('resume')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, 'resume/index.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, '<title>Eren Tuna Açıkbaş</title>')
